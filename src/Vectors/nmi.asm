430a142bce022929df635d6275566421e6a79f64
Tyler O'Brien <hello@tylerobrien.com>
2022-07-24 15:51:54 -0400

    Fix bug causing unwanted background scroll

--- src/Vectors/nmi.asm
+++ src/Vectors/nmi.asm
@@ -10,17 +10,19 @@
 
 .segment "CODE"
 
-.import game_update
+.import game_draw, game_update
 
 .export nmi_handler
 .proc nmi_handler
+    jsr game_update
+
     lda #$00
     sta OAM_ADDR
     lda #$02
     sta OAM_DMA
     lda #$00
 
-    jsr game_update
+    jsr game_draw
 
     sta $2005
 	sta $2005
